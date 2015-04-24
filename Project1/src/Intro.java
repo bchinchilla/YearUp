a1a9c4705321d4f3267fd3f54f1989e830bde8a1
bchinchilla <bchinchilla@atl.yearup.com>
2015-04-24 09:57:24 -0400

    First programming project

---
+++ Project1/src/Intro.java
@@ -0,0 +1,11 @@
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello! I'm Brian Chinchilla. Welcome to the Java class.");
+	}
+
+}
+//If I remove any bracket, it'll say Syntax error and
+/*tell me that I need to insert a bracket*/
