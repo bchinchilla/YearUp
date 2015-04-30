037c258b9c3dd5c4252794752b0fffdb410904e9
unknown <bchinchilla116@PC361.student.YearUp.org>
2015-04-30 16:15:56 -0400

    message

---
+++ Project3/CalculateArea.java
@@ -0,0 +1,23 @@
+
+public class CalculateArea {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double circleArea = CalculateCircleArea(5);
+		System.out.println ("The area of the circle is" + circleArea);
+		
+	
+	}
+
+	static double CalculateCircleArea(int radius) {
+		double area = 3.14 * radius * radius;
+		return area;
+		
+	}
+
+	static double CalculateRectangleArea(int length, int breadth) {
+		double area = length * breadth;
+		return area;
+		
+	}
+}
